9d485ada527e6ae9b00b9997e1b381e3a10fd365
patiltheboy <76406281+patiltheboy@users.noreply.github.com>
2024-08-25 04:19:57 +0530

    update new changes

--- projects/index.js
+++ projects/index.js
@@ -1,27 +1,39 @@
 // Example array data
 const dataArray = [
-    {
-        project: "Project 1",
-        description: "Lorem ipsum dolor sit amet",
-        image: "img/projects/wallicon-interior-pro.webp",
-        url: "https://wallicon.in"
-    },
-    {
-        project: "Project 1",
-        description: "Lorem ipsum dolor sit amet",
-        image: "img/projects/house-of-kalakriti.webp",
-        url: "https://wallicon.in",
-        url2: "https://wallicon.in"
-    },
+  {
+    image: "img/projects/wallicon-interior-pro.webp",
+    project: "Wallicon",
+    skills: [
+      "UX Designer",
+      "UI Designer",
+      "Seller Panel",
+      "Admin Panel",
+      "Figma",
+    ],
+    description:
+      "Wallicon offers a variety of interior products. Sellers can register easily, set up their profile, choose products, and customize prices. They can manage orders and promote their site on Facebook, Instagram, WhatsApp, and other social media platforms.",
+    url: "https://wallicon.in",
+    url2: "https://wallicon.in",
+  },
+  {
+    image: "img/projects/restro-pos-webkul.webp",
+    project: "Restro-POS Case Study",
+    skills: ["UX Designer", "POS Panel", "Figma"],
+    description:
+      "Webkul provides a Restro POS table booking system with a user-friendly interface for fast transactions, featuring reporting, offline orders, payments, table selection, inventory management, staff management, customer insights, and promotions.",
+    url: "https://wallicon.in",
+    url2: "https://wallicon.in",
+  },
 ];
 
 // Get the container where the data will be displayed
-const container = document.getElementById('data-container');
+const container = document.getElementById("data-container");
 
 // Function to map array data to HTML
 function mapArrayToHTML(array) {
-    return array.map(item => {
-        return `
+  return array
+    .map((item) => {
+      return `
            <div class="col-md-6">
             <div class="blog-item wow fadeInUp" data-wow-delay="0.3s">
               <div class="blog-img">
@@ -30,23 +42,33 @@ function mapArrayToHTML(array) {
               <div class="blog-text">
                 <h2>${item.project}</h2>
                 <div class="blog-meta">
-                  <p><i class="far fa-user"></i>Admin</p>
-                  <p><i class="far fa-list-alt"></i>Apps Design</p>
-                  <p><i class="far fa-calendar-alt"></i>01-Jan-2045</p>
-                  <p><i class="far fa-comments"></i>10</p>
+                  ${item?.skills?.map((skill) => `<p>${skill}</p>`).join("")}
                 </div>
-                <p>
+                <p class="truncate">
                   ${item.description}
                 </p>
-                <a target="_blank" class="btn" href="${item.url}" 
-                  >Read More <i class="fa fa-angle-right"></i
-                ></a>
+                 <div class="hero-btn">
+                <a target="_blank" class="btn mt-2" href="${
+                  item.url
+                }">Read More</a>
+                 <a target="_blank" class="btn mt-2" href="${
+                   item.url2
+                 }">View Demo</a>
+                 </div>
               </div>
             </div>
           </div>
         `;
-    }).join('');
+    })
+    .join("");
 }
 
 // Inject the mapped HTML into the container
 container.innerHTML = mapArrayToHTML(dataArray);
+
+{
+  /* <p><i class="far fa-user"></i>Admin</p>
+                  <p><i class="far fa-list-alt"></i>Apps Design</p>
+                  <p><i class="far fa-calendar-alt"></i>01-Jan-2045</p>
+                  <p><i class="far fa-comments"></i>10</p> */
+}
